7b1f5db86e534028f0848edde7ef3fe9cb2aed37
ZUBAER <zubaer.developer@gmail.com>
2026-01-10 14:54:36 +0600

    project setup with tailwindcss and daisyUI

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div data-theme="dark">
+      <h1 className="text-3xl font-bold text-center py-10">My Portfolio</h1>
+    </div>
+  );
+}
+
+export default App;
