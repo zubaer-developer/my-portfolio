30501c99af01dc6c9c9cbd26325796ae3a9dcebe
ZUBAER <zubaer.developer@gmail.com>
2026-01-11 12:57:43 +0600

    hero section structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div data-theme="corporate">
       <Navbar></Navbar>
+      <Hero></Hero>
       <h1 className="text-3xl font-bold text-center py-10">My Portfolio</h1>
     </div>
   );
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,30 @@
-import React from "react";
-
 const Hero = () => {
   return (
-    <div>
-      <h1>This is Hero</h1>
-    </div>
+    <section>
+      <div>
+        {/* left side */}
+        <div>
+          <p>Hello, I am</p>
+          <h1>MD. ZUBAER HOSSAIN</h1>
+          <h2>Full Stack Developer</h2>
+          <p>Short introduction here.....</p>
+          {/* button */}
+          <div>
+            <button>Download Resume</button>
+          </div>
+          {/* socila links */}
+          <div>
+            <a href="#">Github</a>
+            <a href="#">LinkedIn</a>
+          </div>
+
+          {/* Right side */}
+          <div>
+            <img src="../assets/profile.jpg" alt="Profile Photo" />
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
